efba17739c6325448032ce6427ce4ca101d35d15
Duarte <durtemendonca1234@gmail.com>
2026-02-28 02:09:05 +0000

    Squashed commit of the following:
    commit 493dffd4f2a9f24c56d75cc19c3f9ce9b129dec1
    Merge: b86d9c4 4a463ef
    Author: Duarte <durtemendonca1234@gmail.com>
    Date:   Sat Feb 28 02:06:47 2026 +0000
    
        Merge branch 'main' into keys-order
    
    commit b86d9c420da6f237522eb655ee83345ed7f91423
    Author: Duarte <durtemendonca1234@gmail.com>
    Date:   Sat Feb 28 02:05:43 2026 +0000
    
        - (ADD) Base key prefabs
    
    commit 7113d2bfeeadfef03aa15574d5928388d2fa74b4
    Author: Duarte <durtemendonca1234@gmail.com>
    Date:   Sat Feb 28 01:15:10 2026 +0000
    
        - (ADD) Script to randomize character from each corresponding level

---
+++ Assets/Duarte/Keys/BaseKey.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+enum KeyType
+{
+    None,
+    Explode,
+    Hole,
+    Range,
+    Spawner
+}
+
+[RequireComponent(typeof(Collider2D))]
+public class BaseKey : MonoBehaviour
+{
+    // The player layer to check to detect collisions
+    [SerializeField] LayerMask playerLayer;
+
+    [Header("Key Configurations")]
+    // The key unique id should correspond to one of the keyboard keys
+    [SerializeField] string keyID;
+    [SerializeField] KeyType keyType;
+
+    Collider2D collider;
+
+    private void Awake()
+    {
+        collider = GetComponent<Collider2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (keyType == KeyType.None || keyType == KeyType.Spawner) return;
+
+        if (collision.gameObject.layer == playerLayer)
+        {
+            switch (keyType)
+            {
+                case KeyType.Explode:
+                    break;
+                case KeyType.Hole:
+                    break;
+                case KeyType.Range:
+                    break;
+            }
+            print("Key collided with player");
+        }
+    }
+}
