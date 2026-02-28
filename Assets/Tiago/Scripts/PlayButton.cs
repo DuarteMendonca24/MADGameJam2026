c814397bb4dd40933ebf2910fadb18246743732f
tiagofernandes27 <tiago.andre.fernandes77@gmail.com>
2026-02-28 15:37:09 +0000

    Transition and Menu

--- Assets/Tiago/Scripts/PlayButton.cs
+++ Assets/Tiago/Scripts/PlayButton.cs
@@ -1,10 +1,27 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayButton : MonoBehaviour
 {
+    [SerializeField] private GameObject transition;
+    private TransitionController transitionController;
+
+    private void Start()
+    {
+        transitionController = transition.GetComponent<TransitionController>();
+    }
+
     public void ChangeScene(string scene)
     {
+        transition.SetActive(true);
+        transitionController.PlayTransitionInAnimation();
+        Wait(2f);
         SceneManager.LoadScene(scene);
+    }
+
+    private IEnumerator Wait(float secs)
+    {
+        yield return new WaitForSeconds(secs);
     }
 }
